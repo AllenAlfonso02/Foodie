e7675fff82d2db35d5b284bdfe6537a1dfc4863e
kestermbah <156464417+kestermbah@users.noreply.github.com>
2024-06-10 11:34:13 -0400

    Adds the signup information to the login table, and allows a query search to find the infomation in the sign in fetch

--- Foodie/app.py
+++ Foodie/app.py
@@ -1,75 +1,87 @@
 import MySQLdb
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 # Database connection parameters
 host = "localhost"
 user = "root"
-password = "0179849Aa$"
+password = "septons"
+database = "Foodie"
 
-db = MySQLdb.connect(host=host, user=user, passwd=password)
+db = MySQLdb.connect(host=host, user=user, passwd=password, db=database)
 cursor = db.cursor()
 
 @app.route('/')
 def home():
     return render_template('startingPage.html')
 
-@app.route('/signin', methods = ['POST','GET'])
+
+@app.route('/signin', methods=['POST', 'GET'])
 def signin():
     print("in here?")
     if (request.method == 'POST'):
-        print("in here? 1")
+
         user = request.form['userNameOrEmail']
         passWrd = request.form['userPassword']
 
         print(user)
         print(passWrd)
 
-        print("in here? 2")
-
         try:
-            print("in here? 3")
-            #existsQuerry = "SELECT * FROM login WHERE EXISTS (SELECT * FROM login WHERE name = ? AND password = ?)", (user,passWrd)
-            #cursor.execute(existsQuerry, (user, passWrd))
-            
-            cursor.execute("SELECT * FROM login WHERE EXISTS (SELECT * FROM login WHERE name = ? AND password = ?)", (user, passWrd))
-            print("in here? 4")
+
+            cursor.execute("SELECT * FROM login WHERE EXISTS (SELECT * FROM login WHERE name = %s AND password = %s)",
+                           (user, passWrd))
+            result = cursor.fetchone()
+
+            if result:
+                print("Login successful")
+            else:
+                print("Invalid username or password")
+
             return render_template('mainpage.html')
 
-        except:
-            print("in here? 5")
+
+        except MySQLdb.Error as e:
+            print(f"An error occurred: {e}")
             return render_template('singin.html')
 
     elif (request.method == 'GET'):
         print("in here? 6")
         return render_template('signin.html')
 
-@app.route('/signup', methods = ['POST', 'GET'])
+
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    if (request.method == 'POST'):
-        user = request.form['newUserNameOrEmail']
+    if request.method == 'POST':
+        newUser = request.form['newUserNameOrEmail']
         passWrd = request.form['newUserPassword']
         accType = request.form['accountType']
 
-        print(user)
+        print(newUser)
         print(passWrd)
         print(accType)
 
         try:
-            cursor.execute("INSERT INTO login (name, password, type) VALUES (?,?,?)", (user, passWrd, accType))
+            cursor.execute("INSERT INTO login (name, password, type) VALUES (%s, %s, %s)",
+                           (newUser, passWrd, accType))
+            print("Added to the database")
             db.commit()
 
-        except:
+
+        except MySQLdb.Error as e:
             db.rollback()
+            print(f"An error occurred: {e}")
         finally:
             return render_template('signup.html')
-    
+
     else:
         return render_template('signup.html')
+
 
 @app.route('/mainpage')
 def mainpage():
     return render_template('mainpage.html')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
