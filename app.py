f9c140c4529634cb2e7359b4876f3f1fda1b0cba
CodeSponge <Allenalfonso15@gmail.com>
2024-06-19 13:13:20 -0400

    Revert "Merge branch 'main' of https://github.com/AllenAlfonso02/Foodie"
    This reverts commit 753bd2307ea433268bcfc2478b420c8f146464cd, reversing
    changes made to 5d711aa1173e370a81835adcce2c4a9d55a959fe.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import MySQLdb.cursors
 app = Flask(__name__)
 
 currentID = 0
-rootpswd = '0179849Aa$'   #ENTER ROOT PASS HERE
+rootpswd = ''   #ENTER ROOT PASS HERE
 
 # Database connection parameters
 class mySQLClass:
@@ -91,9 +91,6 @@ def signin():
                     db.cursor.execute("SELECT * FROM restaurants WHERE user_id = %s", (pop,))
                     print("1")
                     restaurant = db.cursor.fetchone()
-                    db.close()
-                    db.change(user, passWrd)
-                    db.connect()
                     return redirect(url_for('editrestaurant'))
                 else:
                     print("Unknown user type")
