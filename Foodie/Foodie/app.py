574a0ba0937d41c239b4ab2b27f094f9fb21437b
Yawner01 <wesallen111@gmail.com>
2024-06-18 23:57:13 -0400

    Changes

---
+++ Foodie/Foodie/app.py
@@ -0,0 +1,252 @@
+import MySQLdb
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+
+app = Flask(__name__)
+
+currentID = 0
+
+# Database connection parameters
+host = "localhost"
+user = "root"
+password = "Abeticabted14837!("
+database = "Foodie"
+
+db = MySQLdb.connect(host=host, user=user, passwd=password, db=database)
+cursor = db.cursor()
+
+@app.route('/')
+def home():
+    return render_template('startingPage.html')
+
+
+@app.route('/signin', methods=['POST', 'GET'])
+def signin():
+    print("in here?")
+    if (request.method == 'POST'):
+
+        user = request.form['userNameOrEmail']
+        passWrd = request.form['userPassword']
+
+        print(user)
+        print(passWrd)
+
+        try:
+
+            cursor.execute("SELECT * FROM login WHERE EXISTS (SELECT * FROM login WHERE name = %s AND password = %s)",
+                           (user, passWrd))
+            result = cursor.fetchone()
+
+            if result:
+                "Shows the grants for each user"
+                show_grants_query = "SHOW GRANTS FOR %s@'localhost' USING 'customer_user';"
+                cursor.execute(show_grants_query, (user,))
+                privileges = cursor.fetchall()
+
+                for privilege in privileges:
+                    print(privilege)
+                    "manually input the privileges  "
+                
+                #TESTING HERE
+                cursor.execute("GRANT 'customer_user' TO %s@'localhost';", (user,))
+
+                cursor.execute("FLUSH PRIVILEGES;")
+                "Connects the login user to the database"
+                print("Login successful 0")
+                
+
+                newDB = MySQLdb.connect(host="localhost", user=user, passwd=passWrd, db=database)
+                print("Login successful 1")
+                
+                newCursor = newDB.cursor()
+                print("Login successful 2")
+
+                #newDb.close()
+                return render_template('mainpage.html')
+
+
+            else:
+                print("Invalid username or password")
+                return render_template('startingPage.html')
+
+
+        except MySQLdb.Error as e:
+            print(f"An error occurred: {e}")
+            return render_template('singin.html')
+
+    elif (request.method == 'GET'):
+        return render_template('signin.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        newUser = request.form['newUserNameOrEmail']
+        passWrd = request.form['newUserPassword']
+        accType = request.form['accountType']
+
+        print(newUser)
+        print(passWrd)
+        print(accType)
+        try:
+            cursor.execute("SELECT * FROM login WHERE EXISTS (SELECT * FROM login WHERE name = %s)", (newUser,))
+            result = cursor.fetchone()
+            if result:
+                print("Username already exists")
+                return render_template('signup.html')
+            else:
+                create_user_query = "CREATE USER IF NOT EXISTS %s@'localhost' IDENTIFIED BY %s;"
+                cursor.execute(create_user_query, (newUser, passWrd))
+                grant_user_query = "GRANT 'customer_user' TO %s@'localhost';"
+                cursor.execute("FLUSH PRIVILEGES;")
+                grant_restaurant_query = "GRANT 'restaurant_user' TO %s@'localhost';"
+
+                cursor.execute("INSERT INTO login (name, password, type) VALUES (%s, %s, %s)",
+                               (newUser, passWrd, accType))
+
+                if accType == "User":
+                    cursor.execute(grant_user_query, (newUser,))
+                    cursor.execute("FLUSH PRIVILEGES;")
+                    cursor.execute("SET ROLE customer_user")
+
+                else:
+                    cursor.execute(grant_restaurant_query, (newUser,))
+                    cursor.execute("FLUSH PRIVILEGES;")
+                    cursor.execute("SET ROLE restaurant_user")
+                    
+
+            # Add the new user to the database session
+
+            print("Added to the database")
+            db.commit()
+
+
+        except MySQLdb.Error as e:
+            db.rollback()
+            print(f"An error occurred: {e}")
+        finally:
+            return render_template('signup.html')
+
+    else:
+        return render_template('signup.html')
+
+
+@app.route('/mainpage')
+def mainpage():
+    return render_template('mainpage.html')
+
+@app.route('/loadNext')
+def loadNext():
+    #select statement
+    global currentID; currentID += 1
+    testjson = {"picture" : "https://www.connarchitects.com/wp-content/uploads/2019/09/CONN_Edison-2.jpg",
+                "name" : "Dummy Restaurant",
+                "description" : "This is a json created to test my restaurant.fdhfkjsdhfjdshfkhdskfhkjfsdhjdfhksfhjkhsfkdhfds",
+                }
+    return jsonify(testjson)
+
+@app.route('/addClicked', methods=['POST'])
+def addLiked():
+    if request.method == 'POST':
+        try:
+            user_id = request.json.get('user_id')
+            restaurant_id = request.json.get('restaurant_id')
+
+            if not restaurant_id:
+                return "Failed to get restaurant ID"
+
+            query = """
+                INSERT INTO liked_restaurants (user_id, restaurant_id)
+                VALUES (%s, %s)
+            """
+            cursor.execute(query, (user_id, restaurant_id))
+            db.commit()
+
+            return "Successfully added Restaurant"
+        except MySQLdb.Error as e:
+            db.rollback()
+            return "Unsuccessful insert operation"
+        
+@app.route('/likedrestaurants')
+def liked():
+    return render_template('likedrestaurants.html')
+
+@app.route('/userMenuView', methods = ['POST', 'GET'])
+def showMenu():
+    if (request.method == 'GET'):
+        try:
+            db.row_factory = MySQLdb.Row
+
+            currID = ""
+
+            cursor.execute('SELECT * FROM menu_items WHERE restaurant_id = ?', (currID,))
+
+            rows = cursor.fetchall()
+
+        except:
+            return render_template('userMenuView.html')
+
+    else:
+        return render_template('startingPage.html') 
+    
+@app.route('/addfooditem', methods=['POST'])
+def addfooditem():
+    if request.method == 'POST':
+        # Retrieve form data
+        restaurant_id = request.form['restaurant_id'] 
+        food_name = request.form['food-name']
+        food_description = request.form['food-description']
+        food_price = request.form['food-price']
+        food_url = request.form['food-url']
+
+        try:
+            # Insert food item into menu_items table
+            cursor.execute("""
+                INSERT INTO menu_items (restaurant_id, name, foodurl, description, price)
+                VALUES (%s, %s, %s, %s, %s)
+            """, (restaurant_id, food_name, food_url, food_description, food_price))
+            db.commit()
+            print("Food item added successfully")
+            return render_template('addfooditem.html', message="Food item added successfully")
+        except MySQLdb.Error as e:
+            db.rollback()
+            print(f"An error occurred: {e}")
+            return render_template('addfooditem.html', error="Failed to add food item. Please try again.")
+    return render_template('addfooditem.html')
+
+@app.route('/edituser', methods=['GET', 'POST'])
+def edit_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        phone_number = request.form['phone_number']
+        city = request.form['city']
+        state = request.form['state']
+        postal_code = request.form['postal_code']
+        country = request.form['country']
+
+        cursor.execute("""
+            INSERT INTO customer (username, email, password, first_name, last_name, phone_number, city, state, postal_code, country)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                email=VALUES(email),
+                password=VALUES(password),
+                first_name=VALUES(first_name),
+                last_name=VALUES(last_name),
+                phone_number=VALUES(phone_number),
+                city=VALUES(city),
+                state=VALUES(state),
+                postal_code=VALUES(postal_code),
+                country=VALUES(country);
+        """, (username, email, password, first_name, last_name, phone_number, city, state, postal_code, country))
+        
+        db.commit()
+        return redirect(url_for('index'))
+    
+    return render_template('edituser.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Foodie/Foodie/populate.py
@@ -0,0 +1,65 @@
+import MySQLdb
+
+host = "localhost"
+user = "root"
+password = "Abeticabted14837!("
+
+db = MySQLdb.connect(host=host, user=user, passwd=password)
+cursor = db.cursor()
+msg = 'Test values added successfully.'
+
+cursor.execute('USE Foodie;')
+
+# Adding dummy data
+try:
+    cursor.execute("""INSERT INTO customer (username, email, password, first_name, last_name, phone_number, city, state, postal_code, country) 
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""", 
+                ("bmyers", "b1234@gmail.com", "123", "Bob", "Myers", "8001234567", "Tallahassee", "FL", "32304", "United States"))
+    
+    cursor.execute("""INSERT INTO customer (username, email, password, first_name, last_name, phone_number, city, state, postal_code, country) 
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""",
+                ("jdoe", "janedoe@gmail.com", "password", "Jane", "Doe", "8004110000", "Tallahassee", "FL", "32303", "United States"))
+    
+    cursor.execute("""INSERT INTO customer (username, email, password, first_name, last_name, phone_number, city, state, postal_code, country) 
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""",
+                ("ehkrabs", "mrkrabs@gmail.com", "secretformula", "Mr", "Krabs", "9991234567", "Bikini Bottom", "FL", "32316", "United States"))
+
+    
+    target_username = 'bmyers'
+    cursor.execute("SELECT id FROM customer WHERE username=%s", (target_username,))
+    target_id = cursor.fetchone()
+    
+    cursor.execute("""INSERT INTO restaurants (user_id, name, estabImg, address, city, state, postal_code, country, phone_number, website, cuisine_type, price_range, opening_hours, closing_hours) 
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""", 
+                (target_id, "Chipotle Mexican Grill", "https://fastly.4sqi.net/img/general/width960/37897634_TE7e10vFyZQCr0DRCXU5YOQ66YAtRnhS3lavKaEBFJs.jpg", "1801 W Tennessee St", "Tallahassee", "FL", "32304", "United States", "8502549986", "https://locations.chipotle.com/fl/tallahassee/1801-w-tennessee-st?utm_source=google&utm_medium=yext&utm_campaign=yext_listings", "Mexican", "$10-$20", "10:45", "22:00"))
+
+    target_username = 'ehkrabs'
+    cursor.execute("SELECT id FROM customer WHERE username=%s", (target_username,))
+    target_id = cursor.fetchone()    
+
+    cursor.execute("""INSERT INTO restaurants (user_id, name, estabImg, address, city, state, postal_code, country, phone_number, website, cuisine_type, price_range, opening_hours, closing_hours) 
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""", 
+                (target_id, "Krusty Krab", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/The_Krusty_Krab.png/1167px-The_Krusty_Krab.png?20231003021001", "831 Bottom Feeder Lane", "Bikini Bottom", "FL", "32315", "United States", "1234567890", "https://spongebob.fandom.com/wiki/Krusty_Krab", "American", "$10-$20", "10:00", "22:00"))
+    cursor.execute("SELECT id FROM restaurants WHERE name='Chipotle Mexican Grill'")
+    rest_id = cursor.fetchone()
+    db.commit()
+    cursor.execute("""INSERT INTO liked_restaurants (user_id, restaurant_id) 
+                VALUES (%s,%s);""",
+                (target_id, rest_id))
+    cursor.execute("SELECT id FROM customer WHERE username='jdoe'")
+    target_id = cursor.fetchone()
+    cursor.execute("""INSERT INTO liked_restaurants (user_id, restaurant_id) 
+                VALUES (%s,%s);""", 
+                (target_id, rest_id))
+    cursor.execute("SELECT id FROM restaurants WHERE name='Krusty Krab'")
+    rest_id = cursor.fetchone()
+
+    cursor.execute("""INSERT INTO liked_restaurants (user_id, restaurant_id) 
+                VALUES (%s,%s);""", 
+                (target_id, rest_id))
+except Exception as e:
+    db.rollback()
+    msg = 'Error with inserting dummy values: ' + str(e)
+
+print(msg)
+db.close()
